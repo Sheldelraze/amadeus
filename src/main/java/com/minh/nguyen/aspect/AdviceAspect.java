cdf732c5cc12312e88e5a1a941b0bd45422da45d
Sheldelraze <minh891996@gmail.com>
2018-02-07 23:28:16 +0700

    + Add aspect

---
+++ src/main/java/com/minh/nguyen/aspect/AdviceAspect.java
@@ -0,0 +1,61 @@
+package com.minh.nguyen.aspect;
+
+import com.minh.nguyen.entity.ContestEntity;
+import com.minh.nguyen.entity.ProblemEntity;
+import com.minh.nguyen.exception.NoSuchPageException;
+import com.minh.nguyen.mapper.ContestMapper;
+import com.minh.nguyen.mapper.ProblemMapper;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class AdviceAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(AdviceAspect.class);
+
+    @Autowired
+    private ProblemMapper problemMapper;
+
+    @Autowired
+    private ContestMapper contestMapper;
+
+    @Pointcut("execution(public * com.minh.nguyen.controller.ProblemController.*(..))")
+    public void problemController(){}
+
+    @Pointcut("execution(public * com.minh.nguyen.controller.ContestController.*(..))")
+    public void contestController(){}
+
+    @Pointcut("@annotation(com.minh.nguyen.validator.annotation.CheckNotNullFirst)")
+    public void checkNotNull(){}
+
+    @Before("problemController() && checkNotNull()")
+    public void ensureProblemNotNull(JoinPoint joinPoint){
+        Object[] args = joinPoint.getArgs();
+        Integer pmId = (Integer) args[0];
+        ProblemEntity problemEntity = new ProblemEntity();
+        problemEntity.setId(pmId);
+        problemEntity = problemMapper.selectByPK(problemEntity);
+        if (null == problemEntity){
+            throw new NoSuchPageException("Problem not found!");
+        }
+    }
+
+    @Before("contestController() && checkNotNull()")
+    public void ensureContestNotNull(JoinPoint joinPoint){
+        Object[] args = joinPoint.getArgs();
+        Integer ctId = (Integer) args[0];
+        ContestEntity contestEntity = new ContestEntity();
+        contestEntity.setId(ctId);
+        contestEntity = contestMapper.selectByPK(contestEntity);
+        if (null == contestEntity){
+            throw new NoSuchPageException("Contest not found!");
+        }
+    }
+}
