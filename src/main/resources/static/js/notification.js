a9dec775dba735061fcf165bd0f5a375de066cff
Sheldelraze <minh891996@gmail.com>
2018-03-22 11:40:26 +0700

    + update course now show live notification

--- src/main/resources/static/js/notification.js
+++ src/main/resources/static/js/notification.js
@@ -14,6 +14,66 @@ function onConnect() {
 
 function onMessageNotifyReceived(payload) {
     var message = JSON.parse(payload.body);
+    //notification
+    if (message.type != null && message.content != null) {
+        var noNotification = document.getElementById('noNotification');
+        noNotification.style.display = 'none';
+
+        var notificationDot = document.getElementById('notificationNotify');
+        var notifyCenter = document.getElementById('notificationCenter');
+
+        //show notification
+        notificationDot.classList.remove("hiddenDiv");
+
+        //link
+        var link = document.createElement('a');
+        link.setAttribute('href', '#');
+        link.setAttribute('onclick', 'openInNewTab(\'' + message.link + '\')');
+
+        //icon
+        var iconDiv = document.createElement('div');
+        var icon = document.createElement('i');
+        if (message.type == 1) {
+            iconDiv.className = "btn btn-warning btn-circle";
+            icon.className = "ti-user";
+        } else if (message.type == 2) {
+            iconDiv.className = "btn btn-success btn-circle";
+            icon.className = "fa fa-check";
+        } else if (message.type == 3) {
+            iconDiv.className = "btn btn-danger btn-circle";
+            icon.className = "ti-close";
+        } else if (message.type == 4) {
+            iconDiv.className = "btn btn-danger btn-circle";
+            icon.className = "ti-close";
+        }
+        iconDiv.appendChild(icon);
+
+        //content
+        var contnetDiv = document.createElement('div');
+        contnetDiv.className = "mail-contnet notification-content";
+        var contentName = document.createElement('h5');
+        if (message.type == 1) {
+            contentName.innerHTML = "Xin gia nhập khóa học";
+        } else if (message.type == 2) {
+            contentName.innerHTML = "Xin gia nhập khóa học";
+        } else if (message.type == 3) {
+            contentName.innerHTML = "Xin gia nhập khóa học";
+        } else if (message.type == 4) {
+            contentName.innerHTML = "Loại khỏi khóa học";
+        }
+        var utext = document.createElement('small');
+        utext.style.color = '#99abb4';
+        utext.innerHTML = message.content;
+        contnetDiv.appendChild(contentName);
+        contnetDiv.appendChild(utext);
+
+        //append stuff
+        notifyCenter.appendChild(link);
+        link.appendChild(iconDiv);
+        link.appendChild(contnetDiv);
+    }
+
+    //message
     if (topic != null && message.topic != null && topic != message.topic) {
         var noMessageNotify = document.getElementById('noMessageNotification');
         noMessageNotify.style.display = 'none';
