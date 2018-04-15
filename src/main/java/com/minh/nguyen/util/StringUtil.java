75c65b6de11a8f89bff6b1183cb5ecba7d98fb66
Sheldelraze <minh891996@gmail.com>
2018-04-15 19:43:56 +0700

    + Update announcement form to model

--- src/main/java/com/minh/nguyen/util/StringUtil.java
+++ src/main/java/com/minh/nguyen/util/StringUtil.java
@@ -2,8 +2,6 @@ package com.minh.nguyen.util;
 
 import com.minh.nguyen.constants.Constants;
 
-import java.math.BigInteger;
-
 /**
  * @author Mr.Minh
  * @since 09/01/2018
@@ -11,7 +9,6 @@ import java.math.BigInteger;
  */
 public class StringUtil {
 
-    private static final int SPACE_HEX_IN_ASCII = 0x20;
 
     /**
      * Loại bỏ đường dẫn khi có compile Error, ví dụ:
@@ -156,65 +153,6 @@ public class StringUtil {
         return str == null || str.length() == 0;
     }
 
-    public static boolean isNotEmpty(String str) {
-        return !isEmpty(str);
-    }
-
-    public static boolean isUppercaseAlpha(char c) {
-        return (c >= 'A') && (c <= 'Z');
-    }
-
-    public static boolean isLowercaseAlpha(char c) {
-        return (c >= 'a') && (c <= 'z');
-    }
-
-    public static char toUpperAscii(char c) {
-        if (isLowercaseAlpha(c)) {
-            c -= (char) SPACE_HEX_IN_ASCII;
-        }
-        return c;
-    }
-
-    public static char toLowerAscii(char c) {
-        if (isUppercaseAlpha(c)) {
-            c += (char) SPACE_HEX_IN_ASCII;
-        }
-        return c;
-    }
-
-    public static String camelhumpToUnderline(String str) {
-        final int size;
-        final char[] chars;
-        final StringBuilder sb = new StringBuilder(
-                (size = (chars = str.toCharArray()).length) * 3 / 2 + 1);
-        char c;
-        for (int i = 0; i < size; i++) {
-            c = chars[i];
-            if (isUppercaseAlpha(c)) {
-                sb.append('_').append(toLowerAscii(c));
-            } else {
-                sb.append(c);
-            }
-        }
-        return sb.charAt(0) == '_' ? sb.substring(1) : sb.toString();
-    }
-
-    public static Integer toInt(String str) {
-        if (str != null && str.length() > 0) {
-            return Integer.valueOf(str);
-        } else {
-            return 0;
-        }
-    }
-
-    public static Long toLong(String str) {
-        if (str != null && str.length() > 0) {
-            return Long.valueOf(str);
-        } else {
-            return 0L;
-        }
-    }
-
     public static String getFirstPartOfString(String s, int length) {
         if (null == s) {
             return null;
@@ -226,11 +164,4 @@ public class StringUtil {
         return s;
     }
 
-    public static BigInteger toBigInteger(String str) {
-        if (str != null && str.length() > 0) {
-            return new BigInteger(str);
-        } else {
-            return new BigInteger("0");
-        }
-    }
 }
